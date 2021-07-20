54c7ed3ae074d0e2476ecfe071773d2e4169c88a
lowellausen <lwellausen@gmail.com>
2021-07-20 05:04:40 -0300

    Update robot_info_tracker.py

--- info_tracker/robot_info_tracker.py
+++ info_tracker/robot_info_tracker.py
@@ -1,17 +1,3 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import rclpy, math, json
 from datetime import datetime
 from rclpy.node import Node
@@ -166,10 +152,6 @@ def main(args=None):
     robot_goal_tracker = RobotMovementTracker()
 
     rclpy.spin(robot_goal_tracker)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
 
     robot_goal_tracker.destroy_node()
     rclpy.shutdown()
